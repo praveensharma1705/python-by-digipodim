9caf7e42dd392164a228c21558eb1bd62888acb7
Praveen <praveensharma1705@gmail.com>
2022-10-18 18:30:03 +0530

    operators

---
+++ area.py
@@ -0,0 +1,6 @@
+# wap to calculate area of circul using its circumference
+c=float(input('enter the value of circumference ='))
+r= (c/2)*(7/22)
+area=(22/7)*r*r
+print('the radius of the circle',r)
+print('area of the circle =',area)
